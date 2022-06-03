574f6fdb4b02da537bc7215a49c384867f99c115
Chetan Batra <chetanbatra065@gmail.com>
2022-06-04 00:35:39 +0530

    8 program

---
+++ 8LargestofTwo.py
@@ -0,0 +1,8 @@
+a=float(input("Enter First Number:\n"))
+b=float(input("Enter Second Number:\n"))
+
+if a>b:
+    print("The largest of two numbers is",a)
+
+else:
+    print("The largest of two numbers is",b)    

51e0192497963b93a651f3d1c1c7d316dad68553
Chetan Batra <chetanbatra065@gmail.com>
2022-06-04 00:36:59 +0530

    9 program

---
+++ 9Largestofthree.py
@@ -0,0 +1,13 @@
+a=float(input("Enter First Number:\n"))
+b=float(input("Enter Second Number:\n"))
+c=float(input("Enter Third Number:\n"))
+
+if a>b and b>c :
+    print("The largest of two numbers is",a)
+
+elif b>a and a>c:
+    print("The largest of two numbers is",b)
+
+else:
+    print("The largest of two numbers is",c)        
+

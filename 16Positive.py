b2b37954c0489b43245f20b5548408b796b6b2f7
Chetan Batra <106827928+git-ChetanBatra@users.noreply.github.com>
2022-06-04 01:02:01 +0530

    Update 16Positive.py

--- 16Positive.py
+++ 16Positive.py
@@ -1,7 +1,10 @@
 
-min=int(input("Enter the minimum number\n"))
-max=int(input("Enter the maximum number\n"))
 
-for i in range(min,max+1):
-    if(i>0):
-        print(i)
+
+a=int(input("Enter the number\n"))
+
+
+if(a>=0):
+    print("Positive")
+else:
+     print("Negative")   

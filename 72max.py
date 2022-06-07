62ec2f82284c90a46e018b08498eae8f7930aca9
Chetan Batra <106827928+git-ChetanBatra@users.noreply.github.com>
2022-06-08 00:09:56 +0530

    Add files via upload

---
+++ 72max.py
@@ -0,0 +1,3 @@
+a = [10, 50, 60, 120, 20, 15]
+
+print("The Largest Element in this List is : ", max(a))
---
+++ 77Duplicate.py
@@ -0,0 +1,7 @@
+List = [1, 2, 3, 2, 4, 8, 9, 1, 7, 6, 4, 5]
+print("List Items = ", List)
+
+uniqSet = set(List)
+uniqList = list(uniqSet)
+   
+print("List Items after removing Duplicates = ", uniqList)
